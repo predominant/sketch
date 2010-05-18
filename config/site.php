d28d9d6d5ae7ee786c924dfe0e49b2bc9ad0db51
predominant <graham@grahamweldon.com>
2010-05-19 01:46:13 +1000

    Added site configuration.

---
+++ config/site.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Slavitica Sketch MiniSite
+ *
+ * Copyright (c) 2010 Graham Weldon
+ * Licensed under the LGPL GNU Lesser General Public License
+ * Redistributions of files must retain the above copyright notice
+ *
+ * @author Graham Weldon (http://grahamweldon.com)
+ * @copyright Copyright (c) 2010 Graham Weldon
+ * @license LGPL GNU Lesser General Public License (http://www.gnu.org/licenses/lgpl.html)
+ */
+
+$config['Theme'] = 'sketch1';
+?>
